4a7570ea7422b522707062f5283a88ceebaafd6d
umeyuki <umeyuki1326+github@gmail.com>
2013-12-12 23:10:17 +0900

    delete no need config

--- init.el
+++ init.el
@@ -1,37 +1,10 @@
-; elpa
-(require 'package)
-
-;; package.elでelispを入れるdirectoryの設定
-(setq package-user-dir "~/.emacs.d/elpa/")
-
-;; MELPAを追加
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-(package-initialize)
-
 ; el-get
 (let*
     ((user-emacs-directory
       (substring (or load-file-name "~/.emacs.d/init.el") 0 -7))
      (conf-list (list
                  "el-get.el"
-                 "exec-path.el"
-                 "misc.el"
-                 "window.el"
-                 "git-gutter.el"
-                 "helm.el"
-                 "yasnippet.el"
-                 "auto-complete.el"
-                 "emmet-mode.el"
-                 "ruby-mode.el"
-                 "perl-mode.el"
-                 "popwin.el"
                  "pbcopy.el"
-                 "quickrun.el"
-                 "helm-project.el"
-                 "dash.el"
-                 "color-theme.el"
-                 "server.el"
                 )))
   (dolist (conf conf-list)
     (load (concat user-emacs-directory "conf/" conf))))
