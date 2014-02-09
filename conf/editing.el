33fcd9db9d3a22329257d2037b86260685700c1b
umeyuki <umeyuki1326+github@gmail.com>
2014-02-09 18:11:47 +0900

    utf-8

--- conf/editing.el
+++ conf/editing.el
@@ -67,3 +67,13 @@
 (require 'historyf)
 (global-set-key (kbd "C-l C-b") 'historyf-back)
 (global-set-key (kbd "C-l C-f") 'historyf-forward)
+
+; utf08
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
