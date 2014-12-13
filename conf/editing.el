a8391fa95a763a09c11b238ff09889f02475d512
umeyuki <umeyuki1326+github@gmail.com>
2014-12-13 22:18:48 +0900

    backup file on local

--- conf/editing.el
+++ conf/editing.el
@@ -136,3 +136,15 @@
                     (call-interactively 'ag)
                     (select-window ; select ag buffer
                      (car (my/get-buffer-window-list-regexp "^\\*ag ")))))
+; ghq
+(global-set-key (kbd "M-p") 'helm-ghq)
+
+;; create backup file in ~/.emacs.d/backup
+(setq make-backup-files t)
+(setq backup-directory-alist
+  (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
+    backup-directory-alist))
+
+;; create auto-save file in ~/.emacs.d/backup
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backup/") t)))
