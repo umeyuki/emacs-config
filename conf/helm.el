52fd3aa51610b5cf27eb610a204bb68a30fbea0b
umeyuki <umeyuki1326+github@gmail.com>
2015-01-06 18:49:25 +0900

    add helm-ag

--- conf/helm.el
+++ conf/helm.el
@@ -24,3 +24,9 @@
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 ;; ;;TABで任意補完。選択肢が出てきたらC-nやC-pで上下移動してから決定することも可能
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
+(require 'helm-ag)
+
+(global-set-key (kbd "C-l C-g") 'helm-ag)
+(global-set-key (kbd "C-l ,") 'helm-ag-pop-stack)
+(global-set-key (kbd "C-M-,") 'helm-ag-this-file)
